25a4521cfb62f996ae9a51dae47680dec2721ef1
theodleif <theodleif@googlemail.com>
2016-01-04 11:02:59 +0100

    import division

--- ViktoriaExperiment2.py
+++ ViktoriaExperiment2.py
@@ -1,3 +1,4 @@
+from __future__ import division  # so that 1/3=0.333 instead of 1/3=0
 from psychopy import visual, core, event, gui, sound, data, logging
 from psychopy.constants import *  # things like STARTED, FINISHED
 import random, csv
